65c5d3d5e5a6eec05e67c2ecfefcef9dfb3f7234
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2025-05-31 17:33:42 -0500

    updated name for pygame display window. added debug file and func and settings

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption("Mercurial")
         self.clock = pygame.time.Clock()
-        # running = True
+        self.running = True
     
     def run(self):
-        while True:
+        while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                    self.running = False
                     sys.exit()
 
             self.screen.fill('black')
+            pygame.display.update()
             self.clock.tick(60)
 
 if __name__ == '__main__':
