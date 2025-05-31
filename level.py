6580e692048777815a9f415c256db3b9cf62d6f0
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2025-05-31 17:39:40 -0500

    added and imported settings into main and updated width, height, and fps to imported settings

---
+++ level.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Level:
+    def __init__(self):
+        self.visible_sprites = pygame.sprit.Group()
+        self.obstacle_sprites = pygame.sprit.Group()
+
+    def run(self):
+        pass
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Mercurial")
         self.clock = pygame.time.Clock()
         self.running = True
+
+        self.level = Level
     
     def run(self):
         while self.running:
@@ -18,8 +21,9 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
-            self.clock.tick(60)
+            self.clock.tick(FPS)
 
 if __name__ == '__main__':
     game = Game()
