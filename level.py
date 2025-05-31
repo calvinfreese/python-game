e8ff30bee916d368ba758569a14bed039c445335
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2025-05-31 17:40:50 -0500

    updated typos in pygame.sprite

--- level.py
+++ level.py
@@ -2,8 +2,12 @@ import pygame
 
 class Level:
     def __init__(self):
-        self.visible_sprites = pygame.sprit.Group()
-        self.obstacle_sprites = pygame.sprit.Group()
+        # dispaly surface
+        self.display_surface = pygame.display.get_surface()
+
+        # sprite groups
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
     def run(self):
         pass
