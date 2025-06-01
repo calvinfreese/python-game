ce856a8cae3c01789ccdba0c11f22b925bdbfc48
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2025-06-01 00:33:54 -0500

    added offset so that the camera follows the sprite around as it moves

--- code/level.py
+++ code/level.py
@@ -9,13 +9,12 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite groups
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
 
     def create_map(self):
-        print('CREATING MAP')
         for row_index, row in enumerate(WORLD_MAP):
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
@@ -28,7 +27,22 @@ class Level:
 
     
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
-        # pass
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(100, 200)
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
